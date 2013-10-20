253261543361f8bd712cf7fee2bd739efa47803c
Arron Washington <arron@theradicaledwards.com>
2013-10-20 14:16:15 -0400

    Improve test messaging.

--- test/tilemap/parser_test.dart
+++ test/tilemap/parser_test.dart
@@ -2,12 +2,11 @@ import 'package:unittest/unittest.dart';
 import 'package:citadel/tilemap/parser.dart';
 
 main() {
-  test('Parser.parse', () {
+  test('Parser.parse raises an error when the XML is not in TMX format', () {
     var wrongXml = '<xml></xml>';
 
     expect( ()=> Parser.parse(wrongXml),
         throwsA('XML is not in TMX format'));
-
   });
 }
 

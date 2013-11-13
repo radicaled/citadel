6d1f5bb5527f4f8acdb322c5057843c672c8a824
Arron Washington <arron@theradicaledwards.com>
2013-11-12 23:00:39 -0500

    Some basic prototyping.
    Utilize inflate.js from zlib.js on the client.
    Add links to license, version of file being used.

--- README.md
+++ README.md
@@ -15,3 +15,7 @@ Not a serious effort. Just using it to explore Dart.
 
 
 Assets in /assets/icon contributed by https://github.com/tgstation/-tg-station and inherit its CC BY-SA 3.0 license (http://creativecommons.org/licenses/by-sa/3.0/)
+
+* inflate.js license: MIT
+  * originating repo: https://github.com/imaya/zlib.js / https://github.com/imaya/zlib.js/blob/9c5b4d981529db0f5db3d55f512b25a98ee2dede/bin/inflate.min.js
+  * license file: https://github.com/imaya/zlib.js/blob/afa7c8dac3a561d8b0c1b5f91c59a131d1fb3550/LICENSE
--- web/citadel.dart
+++ web/citadel.dart
@@ -1,17 +1,33 @@
 import 'dart:html';
 import 'package:citadel/tilemap.dart';
+import 'package:stagexl/stagexl.dart';
+import 'package:js/js.dart' as js;
+
+
+Tilemap tilemap;
+var map;
 
 void main() {
-  query("#sample_text_id")
-    ..text = "Click me!"
-    ..onClick.listen(reverseText);
+  var canvas = querySelector('#stage');
+  var stage = new Stage('Test Stage', canvas);
+  
+  var renderLoop = new RenderLoop();
+  
+  renderLoop.addStage(stage);
+  
+  tilemap = new Tilemap(_inflateZlib);
+  
+  var url = "http://127.0.0.1:3030/citadel/test/fixtures/test.tmx";
+
+  // call the web server asynchronously
+  var request = HttpRequest.getString(url).then(parseMap);
+}
+// TODO: I forgot what the Uint8 type is in JS. Uint8?
+List<int> _inflateZlib(List<int> bytes) {
+  var zlib = new js.Proxy(js.context.Zlib.Inflate, js.array(bytes));
+  return zlib.decompress();
 }
 
-void reverseText(MouseEvent event) {
-  var text = query("#sample_text_id").text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.write(text[i]);
-  }
-  query("#sample_text_id").text = buffer.toString();
+parseMap(String xml) {
+  map = tilemap.loadMap(xml);
 }

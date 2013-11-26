9e4b1aea3abba3641724402387fa942a15a10cbb
Arron Washington <arron@theradicaledwards.com>
2013-11-26 00:59:49 -0500

    Add emptyTile constructor.

--- lib/tilemap/tile.dart
+++ lib/tilemap/tile.dart
@@ -6,7 +6,7 @@ class Tile {
   Tileset tileSet;
 
   // Tile global IDs are 1-based
-  int get gid => tileId + (tileSet.gid - 1);
+  int gid;
 
   int width;
   int height;
@@ -14,8 +14,15 @@ class Tile {
   // Optional X / Y locations for the tile.
   int x, y;
 
+  bool get isEmpty { return gid == 0; }
+
   Tile(this.tileId, this.tileSet) {
     width = tileSet.width;
     height = tileSet.height;
+    gid = tileId + (tileSet.gid - 1);
+  }
+
+  Tile.emptyTile() {
+    gid = 0;
   }
 }

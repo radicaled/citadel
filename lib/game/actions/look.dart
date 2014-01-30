0ed5029e504f6e812bb45d982a0da478d0d7ef5d
Arron Washington <arron@theradicaledwards.com>
2014-01-30 17:27:17 -0500

    Better null check.

--- lib/game/actions/look.dart
+++ lib/game/actions/look.dart
@@ -11,7 +11,7 @@ class LookAction extends Action {
     var lookReaction = target.reactions['look'];
     // TODO: should we trigger lookReaction AND emit the description?
     // Or should we just rely on lookReaction to emit the right text?
-    if (lookReaction) {
+    if (lookReaction != null) {
       lookReaction(target, actioneer);
     } else {
       actioneer.emit(target[Description].text);

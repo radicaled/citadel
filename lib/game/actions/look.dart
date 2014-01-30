4477703443f7643d1213ca7a2d14f8de9d9bc3c3
Arron Washington <arron@theradicaledwards.com>
2014-01-30 17:04:11 -0500

    LookAction tries a look reaction, first.

--- lib/game/actions/look.dart
+++ lib/game/actions/look.dart
@@ -8,6 +8,13 @@ class LookAction extends Action {
   }
 
   perform() {
-    actioneer.emit(target[Description].text);
+    var lookReaction = target.reactions['look'];
+    // TODO: should we trigger lookReaction AND emit the description?
+    // Or should we just rely on lookReaction to emit the right text?
+    if (lookReaction) {
+      lookReaction(target, actioneer);
+    } else {
+      actioneer.emit(target[Description].text);
+    }
   }
 }

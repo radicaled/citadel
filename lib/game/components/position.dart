43cf8eea3e56d3a205654611ab92a4fde30c295f
Arron Washington <arron@theradicaledwards.com>
2014-02-02 21:59:00 -0500

    Add some helper methods to common components.

--- lib/game/components/position.dart
+++ lib/game/components/position.dart
@@ -3,4 +3,6 @@ part of components;
 class Position extends Component {
   int x, y, z;
   Position([this.x = 0, this.y = 0, this.z = 0]);
+
+  operator ==(Position other) => this.x == other.x && this.y == other.y && this.z == other.z;
 }
--- lib/game/components/velocity.dart
+++ lib/game/components/velocity.dart
@@ -3,4 +3,9 @@ part of components;
 class Velocity extends Component {
   int x, y;
   Velocity([this.x = 0, this.y = 0]);
+
+  void halt() {
+    x = 0;
+    y = 0;
+  }
 }

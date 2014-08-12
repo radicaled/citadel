ccd7bf558c148e8d499adb6e61c2adfe11e8c4b7
Arron Washington <arron@theradicaledwards.com>
2014-08-12 01:07:41 -0400

    Add better DSL.

--- lib/game_ui/game_ui.dart
+++ lib/game_ui/game_ui.dart
@@ -4,3 +4,36 @@ import 'package:stagexl/stagexl.dart';
 
 part 'button.dart';
 part 'vbox.dart';
+
+typedef _dslFxn(GuiDSL);
+typedef _containerFxn(ContainerDSL);
+typedef BuilderFunction(builder);
+
+class GameGui {
+  static void construct(_dslFxn f) {
+    f(new GuiDSL());
+  }
+}
+
+class GuiDSL {
+  Button button(String text) {
+    return new Button(text);
+  }
+
+  Vbox vbox(int width, int height, BuilderFunction f) {
+    var vbox = new Vbox(width, height);
+    f(new ContainerDSL(vbox));
+    return vbox;
+  }
+}
+
+class ContainerDSL {
+  DisplayObjectContainer container;
+  ContainerDSL(this.container);
+
+  set x(int value) => container.x = value;
+  set y(int value) => container.y = value;
+
+  void add(object) => container.addChild(object);
+  void addAll(List objects) => objects.forEach((o) => container.addChild(o));
+}

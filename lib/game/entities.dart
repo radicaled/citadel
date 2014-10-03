227957e3f3b1de2ad75ab18b65057b934208c1aa
Arron Washington <arron@theradicaledwards.com>
2014-10-03 02:33:47 -0400

    Remove unused export line.

--- lib/game/entities.dart
+++ lib/game/entities.dart
@@ -4,9 +4,6 @@ import 'dart:mirrors';
 import 'dart:async';
 import 'components.dart';
 
-// The only public API anyone needs to worry about here is 'buildEntity'
-export 'entities.dart' show buildEntity;
-
 part 'entities/src/entity_builder.dart';
 part 'entities/src/entity_manager.dart';
 part 'entities/src/entity_behavior.dart';

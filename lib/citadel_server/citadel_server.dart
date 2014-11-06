a0e36248e6d13366f15605afa9ff63279331cf71
Arron Washington <arron@theradicaledwards.com>
2014-11-05 19:48:21 -0500

    Speaking works again.

--- lib/citadel_server/citadel_server.dart
+++ lib/citadel_server/citadel_server.dart
@@ -133,7 +133,7 @@ class CitadelServer {
         ..targetEntityId = ge.payload['target_entity_id']
         ..withEntityId = ge.payload['with_entity_id']
         ..actionName = ge.payload['action_name']
-        ..details.addAll(ge.payload['details'] ? ge.payload['details'] : {});
+        ..details.addAll(ge.payload['details'] != null ? ge.payload['details'] : {});
       intentSystem.intentQueue.add(intent);
     };
   }

4e336eae55a476956e451f45deb01f517a623234
Arron Washington <arron@theradicaledwards.com>
2014-11-12 02:11:04 -0500

    Maul the speaker intent system into handling commands.
    "Speech" prefixed with a / now gets special handling.

--- lib/citadel_server/src/systems/speak_intent_system.dart
+++ lib/citadel_server/src/systems/speak_intent_system.dart
@@ -2,7 +2,26 @@ part of citadel_server;
 
 void speakIntentSystem(Intent intent) {
   var speaker = findEntity(intent.invokingEntityId);
-  var text = intent.details['text'];
-  if (text == null) { return; }
-  speaker.use(Name, (name) => world.emit('${name.text}: $text', nearEntity: speaker));
+  String text = intent.details['text'];
+  if (text == null || text.isEmpty) { return; }
+  var _speak = () =>
+    speaker.use(Name, (name) => world.emit('${name.text}: $text', nearEntity: speaker));
+
+  var _command = () {
+    switch(text) {
+      case '/help':
+        world.emit('Commands: none available yet', toEntity: speaker);
+      break;
+      default:
+        world.emit('Unrecognized command; try /help', toEntity: speaker);
+      break;
+    }
+  };
+
+  if (text[0] == '/' && text.length > 2) {
+    _command();
+  } else {
+    _speak();
+  }
+
 }
